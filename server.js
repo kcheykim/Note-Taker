353e13801b3d275fd582b202724e51914a06c7df
kcheykim <kosalcheykim@gmail.com>
2021-11-27 06:46:06 -0800

    Add Heroku

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notest } = require('./db/db');
+const { notes } = require('./db/db');
 
 
 
 
 
 app.get('/api/db', (req, res) => {
-    res.send('Hello!');
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
 });
 
 app.listen(PORT, () => {
