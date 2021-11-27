d4840db16d090cfad3b305b722444fbdcad995d5
kcheykim <kosalcheykim@gmail.com>
2021-11-27 15:54:41 -0800

    getting the application to output the front-end files

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 const { notes } = require('./db/db');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public')); //make front end files like css and javascript available
+app.use(express.urlencoded({ extended: true })); //parse incoming string or array data
+app.use(express.json()); //parse incoming JSON data
+
+function createNewNotes(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') { return false; }
+    if (!note.text || typeof note.text !== 'string') { return false; }
+    return true;
+}
 
 
-
-
-app.get('/api/db', (req, res) => {
+app.get('/api/notes', (req, res) => {
     let results = notes;
-    console.log(req.query)
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString(); // set id based on what the next index of the array will be
+    const note = createNewNote(req.body, notes); // add note to json file and note array in this function
+    res.json(note);
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
