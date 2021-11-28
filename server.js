1b94e9b3cd3a01bcff560ea3176f4a3a095de240
kcheykim <kosalcheykim@gmail.com>
2021-11-27 18:59:17 -0800

    getting the the app to read json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require('./db/db');
+const { notes } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,32 +10,47 @@ app.use(express.static('public')); //make front end files like css and javascrip
 app.use(express.urlencoded({ extended: true })); //parse incoming string or array data
 app.use(express.json()); //parse incoming JSON data
 
-function createNewNotes(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}
+// function createNewNotes(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+//     return note;
+// }
 
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') { return false; }
-    if (!note.text || typeof note.text !== 'string') { return false; }
-    return true;
-}
+// function validateNote(note) {
+//     if (!note.title || typeof note.title !== 'string') { return false; }
+//     if (!note.text || typeof note.text !== 'string') { return false; }
+//     return true;
+// }
 
+// app.get('/api/notes', (req, res) => {
+//     // let results = notes;
+//     // res.json(results);
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
-});
+//     fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+//             if (err) { console.log(err) } else {
+//                 const parsedNotes = JSON.parse(data);
+//                 res.json(parsedNotes)
+//             }
+//         })
+//         // Send a message to the user
+//     res.json(`${req.method} request received for notes`);
+
+//     // Log our notes to the terminal
+//     console.info(`${req.method} request received to get notes`);
+// });
 
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString(); // set id based on what the next index of the array will be
     const note = createNewNote(req.body, notes); // add note to json file and note array in this function
     res.json(note);
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 app.get('/notes', (req, res) => {
